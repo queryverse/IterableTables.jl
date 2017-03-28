d690b0d1274763d3106648ddc3b306a6fee48aaa
autodocs <autodocs>
2017-03-28 23:13:31 +0000

    build based on 04022ae

---
+++ latest/search_index.js
@@ -0,0 +1,35 @@
+var documenterSearchIndex = {"docs": [
+
+{
+    "location": "index.html#",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "page",
+    "text": ""
+},
+
+{
+    "location": "index.html#Introduction-1",
+    "page": "Introduction",
+    "title": "Introduction",
+    "category": "section",
+    "text": ""
+},
+
+{
+    "location": "index.html#Overview-1",
+    "page": "Introduction",
+    "title": "Overview",
+    "category": "section",
+    "text": "[TODO]"
+},
+
+{
+    "location": "index.html#Installation-1",
+    "page": "Introduction",
+    "title": "Installation",
+    "category": "section",
+    "text": "This package only works on julia 0.5 and newer. You can add it with:Pkg.add(\"IterableTables\")"
+},
+
+]}
