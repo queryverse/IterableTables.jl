eda537df0c0c9a2a74cb5c78f2d30ff18881c445
David Anthoff <anthoff@berkeley.edu>
2017-03-28 14:57:56 -0700

    Add core functionality from Query

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,10 @@
 using IterableTables
+using NamedTuples
+using SimpleTraits
 using Base.Test
 
-# write your own tests here
-@test 1 == 2
+table_array = [@NT(a=1), @NT(a=2)]
+other_array = [1,2,3]
+
+@test istrait(IsIterableTable{typeof(table_array)})
+@test !istrait(IsIterableTable{typeof(other_array)})
