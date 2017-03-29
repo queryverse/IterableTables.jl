ec3e87550bc301a780e6ae54b5ce15b49c4884e4
David Anthoff <anthoff@berkeley.edu>
2017-03-29 09:57:38 -0700

    Make IndexedTables an iterable table

--- src/integrations/indexedtables.jl
+++ src/integrations/indexedtables.jl
@@ -1,70 +1,73 @@
 @require IndexedTables begin
-using IndexedTables: NDSparse
+using IndexedTables: IndexedTable
 
-immutable NDSparseIterator{T, S<:NDSparse}
+immutable IndexedTableIterator{T, S<:IndexedTable}
     source::S
 end
 
-immutable IndexedTablesRow{TIndex,TValue}
-    index::TIndex
-    value::TValue
-end
+@traitimpl IsIterable{IndexedTables.IndexedTable}
+@traitimpl IsIterableTable{IndexedTables.IndexedTable}
 
-@traitimpl IsIterable{IndexedTables.NDSparse}
+function getiterator{S<:IndexedTable}(source::S)
+    col_expressions = Array{Expr,1}()
+    columns_tuple_type = Expr(:curly, :Tuple)
 
-function getiterator{S<:NDSparse}(source::S)
-    TValue = S.parameters[1]
     if S.parameters[3]<:NamedTuples.NamedTuple
-        col_expressions = Array{Expr,1}()
-        columns_tuple_type = Expr(:curly, :Tuple)
-
         for (i,fieldname) in enumerate(fieldnames(S.parameters[3]))
             col_type = S.parameters[2].parameters[i]
             push!(col_expressions, Expr(:(::), fieldname, col_type))
             push!(columns_tuple_type.args, col_type)
         end
-        t_expr = NamedTuples.make_tuple(col_expressions)
-        TIndex = eval(NamedTuples, t_expr)
     else
-        TIndex = S.parameters[2]
+        error("The keys of the IndexedTable need to have names.")
     end
 
-    e_df = NDSparseIterator{IndexedTablesRow{TIndex,TValue},S}(source)
+    if S.parameters[1]<:NamedTuples.NamedTuple
+        for (i,fieldname) in enumerate(fieldnames(S.parameters[1]))
+            col_type = S.parameters[1].parameters[i]
+            push!(col_expressions, Expr(:(::), fieldname, col_type))
+            push!(columns_tuple_type.args, col_type)
+        end
+    else
+        error("The values of the IndexedTable need to have names.")
+    end
+
+    t_expr = NamedTuples.make_tuple(col_expressions)
+    T = eval(NamedTuples, t_expr)
+
+    e_df = IndexedTableIterator{T,S}(source)
 
     return e_df
 end
 
-Base.eltype{T,S<:NDSparse}(iter::NDSparseIterator{T,S}) = T
+Base.eltype{T,S<:IndexedTable}(iter::IndexedTableIterator{T,S}) = T
 
-Base.eltype{T,S<:NDSparse}(iter::Type{NDSparseIterator{T,S}}) = T
+Base.eltype{T,S<:IndexedTable}(iter::Type{IndexedTableIterator{T,S}}) = T
 
-function Base.start{T,S<:NDSparse}(iter::NDSparseIterator{T,S})
+function Base.start{T,S<:IndexedTable}(iter::IndexedTableIterator{T,S})
     return 1
 end
 
-@generated function Base.next{T,S<:NDSparse}(iter::NDSparseIterator{T,S}, state)
-    if T.parameters[1]<:NamedTuples.NamedTuple
-        constructor_call = Expr(:call, :IndexedTablesRow, Expr(:call,T.parameters[1]),:(iter.source.data[row]))
-        for i in 1:length(S.parameters[2].parameters)
-            push!(constructor_call.args[2].args, :( iter.source.index.columns[$i][row] ))
-        end
-    else
-        constructor_call = Expr(:call, :IndexedTablesRow,:((1,)),:(iter.source.data[row]))
-        constructor_call.args[2].args[1] = :( iter.source.index.columns[1][row] )
-        for i in 2:length(S.parameters[2].parameters)
-            push!(constructor_call.args[2].args, :( iter.source.index.columns[$i][row] ))
+@generated function Base.next{T,S<:IndexedTable}(iter::IndexedTableIterator{T,S}, state)
+    constructor_call = Expr(:call, :($T))
+
+    index_of_first_data_element = length(S.parameters[3].parameters) + 1
+    for i in 1:length(T.parameters)
+        if i<index_of_first_data_element
+            push!(constructor_call.args, :( iter.source.index.columns[$i][row] ))
+        else
+            push!(constructor_call.args, :( iter.source.data[row][$(i-index_of_first_data_element+1)] ))
         end
     end
 
     quote
-        source = iter.source
         row = state
         a = $constructor_call
         return a, state+1
-    end
+    end    
 end
 
-function Base.done{T,S<:NDSparse}(iter::NDSparseIterator{T,S}, state)
+function Base.done{T,S<:IndexedTable}(iter::IndexedTableIterator{T,S}, state)
     return state>length(iter.source)
 end
 
