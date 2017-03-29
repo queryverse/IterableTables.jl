ee68efd9d172cc95cb168b14927ae6c9be238fae
David Anthoff <anthoff@berkeley.edu>
2017-03-28 17:32:23 -0700

    Fix bug in DataTables test

--- test/test_integration_datatables.jl
+++ test/test_integration_datatables.jl
@@ -22,23 +22,8 @@ source_array = [@NT(a=1,b=1.,c="A"), @NT(a=2,b=2.,c="B"), @NT(a=3,b=3.,c="C")]
 dt = DataTable(source_array)
 
 @test size(dt) == (3,3)
-@test !isnull(dt[1,:a])
-@test !isnull(dt[1,:b])
-@test !isnull(dt[1,:c])
-@test !isnull(dt[2,:a])
-@test !isnull(dt[2,:b])
-@test !isnull(dt[2,:c])
-@test !isnull(dt[3,:a])
-@test !isnull(dt[3,:b])
-@test !isnull(dt[3,:c])
-@test get(dt[1,:a]) == 1
-@test get(dt[2,:a]) == 2
-@test get(dt[3,:a]) == 3
-@test get(dt[1,:b]) == 1.
-@test get(dt[2,:b]) == 2.
-@test get(dt[3,:b]) == 3.
-@test get(dt[1,:c]) == "A"
-@test get(dt[2,:c]) == "B"
-@test get(dt[3,:c]) == "C"
+@test dt[:a] == [1,2,3]
+@test dt[:b] == [1.,2.,3.]
+@test dt[:c] == ["A","B","C"]
 
 end
