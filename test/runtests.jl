610d8b0f80308f219b79f51021d48f1390f57586
David Anthoff <anthoff@berkeley.edu>
2017-03-30 10:00:45 -0700

    Disable some tests on julia 0.6

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,7 +1,22 @@
 using IterableTables
-using NamedTuples
 using SimpleTraits
+using NamedTuples
+using DataFrames
+using DataTables
+using DataStreams
+using CSV
+using SQLite
+using TimeSeries
+using StatsModels
+using TypedTables
+using VegaLite
 using Base.Test
+
+if VERSION < v"0.6.0-"
+    using Feather
+    using IndexedTables
+    using Gadfly
+end
 
 @testset "IterableTables" begin
 
@@ -18,11 +33,14 @@ end
 include("test_integration_dataframes.jl")
 include("test_integration_datastreams.jl")
 include("test_integration_datatables.jl")
-include("test_integration_gadfly.jl")
-include("test_integration_indexedtables.jl")
 include("test_integration_statsmodels.jl")
 include("test_integration_timeseries.jl")
 include("test_integration_typedtables.jl")
 include("test_integration_vegalite.jl")
+
+if VERSION < v"0.6.0-"
+    include("test_integration_gadfly.jl")
+    include("test_integration_indexedtables.jl")
+end
 
 end
