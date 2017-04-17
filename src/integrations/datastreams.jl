e6f20538dceb0e431a46234b81486e68d887852e
David Anthoff <anthoff@berkeley.edu>
2017-04-17 11:05:54 -0700

    Add conversion IterableTables -> DataStreams source

--- src/integrations/datastreams.jl
+++ src/integrations/datastreams.jl
@@ -96,4 +96,102 @@ function Base.done{T, S<:DataStreams.Data.Source, TC,TSC}(iter::DataStreamIterat
     return Data.isdone(iter.source,state,1)
 end
 
+# DataStreams Source
+
+type DataStreamSource{TSource,TE} <: Data.Source
+    schema::Data.Schema
+    data::TSource
+    iterate_state
+    current_row::Int
+    current_val::TE
+    function DataStreamSource(schema, data)
+        x = new(schema, data)
+        x.current_row = 0
+        return x
+    end
+end
+
+function Data.isdone{TSource,TE}(source::DataStreamSource{TSource,TE}, row, col)
+    row==source.current_row || row==source.current_row+1 || error()
+
+    if source.current_row==0
+        source.iterate_state = start(source.data)
+    end
+
+    if row==source.current_row+1
+        if done(source.data, source.iterate_state)
+            return true
+        else
+            (source.current_val, source.iterate_state) = next(source.data, source.iterate_state)
+            source.current_row = source.current_row+1
+        end
+    end
+
+    return false
+end
+
+Data.streamtype{T<:DataStreamSource}(::Type{T}, ::Type{Data.Field}) = true
+
+function Data.streamfrom{T}(source::DataStreamSource, ::Type{Data.Field}, ::Type{T}, row, col)
+    row==source.current_row || row==source.current_row+1 || error()
+
+    if source.current_row==0
+        source.iterate_state = start(source.data)
+    end
+
+    if row==source.current_row+1
+        (source.current_val, source.iterate_state) = next(source.data, source.iterate_state)
+        source.current_row = source.current_row+1
+    end
+
+    return source.current_val[col]::T
+end
+
+function Data.streamfrom{T}(source::DataStreamSource, ::Type{Data.Field}, ::Type{Nullable{T}}, row, col)
+    row==source.current_row || row==source.current_row+1 || error()
+
+    if source.current_row==0
+        source.iterate_state = start(source.data)
+    end
+
+    if row==source.current_row+1
+        (source.current_val, source.iterate_state) = next(source.data, source.iterate_state)
+        source.current_row = source.current_row+1
+    end
+
+    val = source.current_val[col]
+
+    if typeof(val) <: Nullable
+        if isnull(val)
+            return Nullable{T}()
+        else
+            return Nullable{T}(get(val))
+        end
+    else
+        return Nullable{T}(val)
+    end
+end
+
+function Data.schema(source::DataStreamSource)
+    return source.schema
+end
+
+function Data.schema(source::DataStreamSource, ::Type{Data.Field})
+    return Data.schema(source)
+end
+
+function get_datastreams_source{S}(source::S)
+    isiterabletable(source) || error()
+
+    iter = getiterator(source)
+
+    column_types = IterableTables.column_types(iter)
+    column_names = IterableTables.column_names(iter)
+
+
+    schema = Data.Schema(column_names, column_types, -1)
+    source = DataStreamSource{typeof(iter),eltype(iter)}(schema, iter)
+    return source
+end
+
 end
