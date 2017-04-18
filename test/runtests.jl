382f020abaad2d1663d533ca41fe12a9578910fd
David Anthoff <anthoff@berkeley.edu>
2017-04-17 22:34:20 -0700

    Add tests for differential equation integration

--- test/runtests.jl
+++ test/runtests.jl
@@ -13,11 +13,19 @@ using TypedTables
 using VegaLite
 using PooledArrays
 using IndexedTables
+using ParameterizedFunctions
 using Base.Test
 
 if VERSION < v"0.6.0-"    
     using Gadfly
 end
+
+# This defines a type needed by the differential equations
+# test, and that cannot happen in a testset
+f_2dlinear_named = @ode_def LotkaVolterra begin
+  dx = a*x - b*x*y
+  dy = -c*y + d*x*y
+end a=>1.5 b=>1 c=3 d=1
 
 @testset "IterableTables" begin
 
@@ -44,6 +52,7 @@ include("test_integration_timeseries.jl")
 include("test_integration_typedtables.jl")
 include("test_integration_vegalite.jl")
 include("test_integration_indexedtables.jl")
+include("test_integration_differentialequations.jl")
 
 if VERSION < v"0.6.0-"
     include("test_integration_gadfly.jl")    
