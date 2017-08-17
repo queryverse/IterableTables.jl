5b8e80828c356f7231a6cc02ccf322bd0b55d5c5
David Anthoff <anthoff@berkeley.edu>
2017-08-17 14:50:02 -0700

    Remove tests that now live in TableTraits.jl

--- test/runtests.jl
+++ test/runtests.jl
@@ -30,28 +30,6 @@ end
 
 @testset "IterableTables" begin
 
-@testset "Core" begin
-
-table_array = [@NT(a=1), @NT(a=2)]
-other_array = [1,2,3]
-
-@test isiterabletable(table_array)
-@test !isiterabletable(other_array)
-
-@test_throws ErrorException getiterator(MyType())
-
-iter = getiterator(table_array)
-@test TableTraits.column_names(iter) == [:a]
-@test TableTraits.column_types(iter) == [Int]
-@test TableTraits.column_count(iter) == 1
-
-iter2 = getiterator(other_array)
-@test_throws ErrorException TableTraits.column_names(iter2)
-@test_throws ErrorException TableTraits.column_types(iter2)
-@test_throws ErrorException TableTraits.column_count(iter2)
-
-end
-
 include("test_integration_dataframes.jl")
 include("test_integration_datastreams.jl")
 include("test_integration_datatables.jl")
