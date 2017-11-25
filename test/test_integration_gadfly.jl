74b3dc2caf6df1d5f125beec3f2fd58e65dbca4e
David Anthoff <anthoff@berkeley.edu>
2017-11-25 12:14:22 -0800

    Fix Gadfly test

--- test/test_integration_gadfly.jl
+++ test/test_integration_gadfly.jl
@@ -8,7 +8,7 @@ using Base.Test
 
 source_dt = DataTable(a=[1,2,3], b=[4,2,6])
 
-p = plot(source_dt, x=:a, y=:b, Geom.line)
+p = Gadfly.plot(source_dt, x=:a, y=:b, Geom.line)
 
 @test isa(p, Gadfly.Plot)
 
