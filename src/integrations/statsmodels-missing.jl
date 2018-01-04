6d554a2a7e1062d1534c6a37fdaca56adfa1019d
Pietro Vertechi <pietro.vertechi@neuro.fchampalimaud.org>
2018-01-04 14:35:29 +0000

    simplify statsmodels

--- src/integrations/statsmodels-missing.jl
+++ src/integrations/statsmodels-missing.jl
@@ -8,18 +8,8 @@ function StatsModels.ModelFrame(f::StatsModels.Formula, d; kwargs...)
     StatsModels.ModelFrame(f, DataFrames.DataFrame(d); kwargs...)
 end
 
-function StatsBase.fit{T<:StatsBase.StatisticalModel}(::Type{T}, f::StatsModels.Formula, source, args...; contrasts::Dict = Dict(), kwargs...)
-    isiterabletable(source) || error()
-    mf = StatsModels.ModelFrame(f, source, contrasts=contrasts)
-    mm = StatsModels.ModelMatrix(mf)
-    y = StatsBase.model_response(mf)
-    StatsModels.DataFrameStatisticalModel(StatsBase.fit(T, mm.m, y, args...; kwargs...), mf, mm)
-end
-
-function StatsBase.fit{T<:StatsBase.RegressionModel}(::Type{T}, f::StatsModels.Formula, source, args...; contrasts::Dict = Dict(), kwargs...)
-    isiterabletable(source) || error()
-    mf = StatsModels.ModelFrame(f, source, contrasts=contrasts)
-    mm = StatsModels.ModelMatrix(mf)
-    y = StatsBase.model_response(mf)
-    StatsModels.DataFrameRegressionModel(StatsBase.fit(T, mm.m, y, args...; kwargs...), mf, mm)
+function StatsBase.fit(::Type{T}, f::StatsModels.Formula, d, args...; contrasts::Dict = Dict(), kwargs...) where
+                       T<:Union{StatsBase.StatisticalModel, StatsBase.RegressionModel}
+    isiterabletable(d) || error()
+    StatsBase.fit(T, f, DataFrames.DataFrame(d), args...; contrasts = contrasts, kwargs...)
 end
