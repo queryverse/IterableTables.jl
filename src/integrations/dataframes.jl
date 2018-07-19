8b076879a45de3aaa0aa1424e64813b6a1ece8d7
David Anthoff <anthoff@berkeley.edu>
2018-07-19 10:51:10 +0300

    Fix DataFrames integration

--- src/integrations/dataframes.jl
+++ src/integrations/dataframes.jl
@@ -4,7 +4,7 @@ IteratorInterfaceExtensions.isiterable(x::DataFrames.DataFrame) = true
 TableTraits.isiterabletable(x::DataFrames.DataFrame) = true
 
 function TableTraits.getiterator(df::DataFrames.DataFrame)
-    return TableTraitsUtils.create_tableiterator(df.columns, names(df))
+    return TableTraitsUtils.create_tableiterator(getfield(df, :columns), names(df))
 end
 
 # # Sink
