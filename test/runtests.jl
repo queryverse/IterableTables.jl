d76259b6cfc4bc8755ccdc8f84e82d4586d101b9
David Anthoff <anthoff@berkeley.edu>
2018-08-11 14:47:55 -0700

    Disable statsmodel integration test for now

--- test/runtests.jl
+++ test/runtests.jl
@@ -7,7 +7,8 @@ end
 
 include("test_integration_dataframes.jl")
 # include("test_integration_datastreams.jl")
-include("test_integration_statsmodels.jl")
+# TODO Reenable once GLM is 0.7 compatible
+# include("test_integration_statsmodels.jl")
 # include("test_integration_timeseries.jl")
 # include("test_integration_temporal.jl")
 # include("test_integration_juliadb.jl")
