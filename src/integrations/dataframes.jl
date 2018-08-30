940a08f1bce0f4c93ecce917e6f92dcca70ad425
quinnj <quinn.jacobd@gmail.com>
2018-08-29 22:25:40 -0600

    Remove DataFrames catchall constructor for transition to DataFrames.jl package

--- src/integrations/dataframes.jl
+++ src/integrations/dataframes.jl
@@ -6,21 +6,3 @@ TableTraits.isiterabletable(x::DataFrames.DataFrame) = true
 function TableTraits.getiterator(df::DataFrames.DataFrame)
     return TableTraitsUtils.create_tableiterator(getfield(df, :columns), names(df))
 end
-
-# # Sink
-
-function _DataFrame(x)
-    cols, names = create_columns_from_iterabletable(x, na_representation=:missing)
-
-    return DataFrames.DataFrame(cols, names)
-end
-
-DataFrames.DataFrame(x::AbstractVector{T}) where {T<:NamedTuple} = _DataFrame(x)
-
-function DataFrames.DataFrame(x)
-    if TableTraits.isiterabletable(x)
-        return _DataFrame(x)
-    else
-        return convert(DataFrames.DataFrame, x)
-    end
-end
