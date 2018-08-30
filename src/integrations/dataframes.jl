c40bed8dc2c997cbda29558ac18b63161a8b927d
quinnj <quinn.jacobd@gmail.com>
2018-08-29 22:27:11 -0600

    Remove getiterator call in preparation for moving to DataFrames.jl

--- src/integrations/dataframes.jl
+++ src/integrations/dataframes.jl
@@ -2,7 +2,3 @@
 
 IteratorInterfaceExtensions.isiterable(x::DataFrames.DataFrame) = true
 TableTraits.isiterabletable(x::DataFrames.DataFrame) = true
-
-function TableTraits.getiterator(df::DataFrames.DataFrame)
-    return TableTraitsUtils.create_tableiterator(getfield(df, :columns), names(df))
-end
