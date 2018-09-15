911e3bd774c0ee3c3f6c3eb4dc74a1b0956250aa
David Anthoff <anthoff@berkeley.edu>
2018-09-14 21:38:58 -0700

    Load DataFrames integration only for old versions of DataFrames

--- src/IterableTables.jl
+++ src/IterableTables.jl
@@ -10,7 +10,9 @@ include("integrations/generators.jl")
 # include("integrations/temporal.jl")
 
 function __init__()
-    @require DataFrames="a93c6f00-e57d-5684-b7b6-d8193f3e46c0" include("integrations/dataframes.jl")
+    @require DataFrames="a93c6f00-e57d-5684-b7b6-d8193f3e46c0" if !isdefined(DataFrames, :Tables)
+        include("integrations/dataframes.jl")
+    end
     @require StatsModels="3eaba693-59b7-5ba5-a881-562e759f1c8d" include("integrations/statsmodels.jl")
 end
 
